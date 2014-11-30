1e52eb013c5ae7226f99b81c4d8b3f516746f021
k5t4j5 <boogs19.kj@gmail.com>
2014-11-30 01:45:21 -0500

    Update Sprint fingerprint to 4.4.4, radio trial and error
    Change-Id: If6ed4bf9131c38e6bfeeea486bf732b9fb3695bb

--- init/init_m8.c
+++ init/init_m8.c
@@ -80,14 +80,15 @@ void vendor_load_properties()
     } else if (strstr(bootmid, "0P6B70000")) {
         /* m8spr (m8whl) */
         cdma_properties("1", "8", "310120", "Sprint");
-        property_set("ro.build.fingerprint", "htc/sprint_wwe/htc_m8whl:4.4.2/KOT49H/326598.8:user/release-keys");
-        property_set("ro.build.description", "1.54.651.8 CL326598 release-keys");
+        property_set("ro.build.fingerprint", "htc/sprint_wwe/htc_m8whl:4.4.4/KTU84P/393205.2:user/release-keys");
+        property_set("ro.build.description", "3.30.654.2 CL393205 release-keys");
         property_set("ro.product.device", "m8spr");
         property_set("ro.build.product", "m8spr");
         property_set("telephony.sms.pseudo_multipart", "1");
         property_set("ro.ril.oem.ecclist", "911");
         property_set("ro.ril.enable.pre_r8fd=1", "1");
         property_set("persist.radio.apm_sim_not_pwdn", "1");
+        property_set("ro.ril.scan.selection.rat", "1");
         property_set("ro.ril.enable.sdr", "0");
         property_set("ro.ril.set.mtusize", "1422");
     } else {
